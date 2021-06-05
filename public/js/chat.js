b3dc0981b0fdcc2891bd091e0174033cb69cd8bd
long bui <blong1102@gmail.com>
2021-06-05 18:23:07 +0700

    render location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,58 @@
 const socket = io()
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButtton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $message = document.querySelector('#message')
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
 socket.on('message', (message) => {
-    console.log(message)
+    // console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $message.insertAdjacentHTML('beforeend', html)
 })
-document.querySelector('#sendMessage').addEventListener('click', (target) => {
-    const message = document.querySelector('#message').value;
-    socket.emit('sent message', message)
+
+socket.on('locationMessage', (locationLink) => {
+    const html = Mustache.render(locationTemplate, {
+        locationLink
+    })
+    $message.insertAdjacentHTML('beforeend', html)
+})
+
+
+$messageForm.addEventListener('submit', (target) => {
+    target.preventDefault()
+
+    const message = $messageFormInput.value
+    $messageFormButtton.setAttribute('disabled', 'disabled')
+
+    socket.emit('sendMessage', message, (err) => {
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        $messageFormButtton.removeAttribute('disabled')
+        if (err) {
+            return console.log(err)
+        }
+        console.log('Message delivered')
+    })
+})
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Your browser does not support geolocation')
+    }
+    $locationButton.setAttribute('disabled', 'disabeled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            long: position.coords.latitude,
+            lat: position.coords.longitude
+        }, () => {
+            console.log('Shared location!')
+            $locationButton.removeAttribute('disabled')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,29 +2,41 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
-
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-
 const publicPath = path.join(__dirname, '/../public')
 app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
+    const filter = new Filter()
     socket.emit('message', 'Welcome!')
+
     socket.broadcast.emit('message', 'A user has join!')
-    socket.on('sent message', (message) => {
+
+    socket.on('sendMessage', (message, callback) => {
+        message = filter.clean(message)
         io.emit('message', message)
+        callback()
     })
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (position, callback) => {
+        socket.emit('locationMessage',
+            `https://google.com/maps?q=${position.long},${position.lat}`
+        )
+        callback()
     })
 })
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+    console.log(`listening on port ${PORT}👍`)
 })
